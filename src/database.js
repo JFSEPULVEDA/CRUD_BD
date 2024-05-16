1c733ceec0262512c49672e7d04cf22199ce861a
JFSEPULVEDA <127802988+JFSEPULVEDA@users.noreply.github.com>
2024-05-15 23:27:00 -0500

    Conexion a mongo cloud

--- src/database.js
+++ src/database.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/stech', {
-});
-
-
-/*mongoose.connect('mongodb://mongo:bYLBDKiOzJKMfcBJfneqGddQgLJcrtFh@roundhouse.proxy.rlwy.net:29120', {
+/*mongoose.connect('mongodb://localhost:27017/stech', {
 });*/
+
+mongoose.connect('mongodb://mongo:bYLBDKiOzJKMfcBJfneqGddQgLJcrtFh@roundhouse.proxy.rlwy.net:29120', {
+});
 
