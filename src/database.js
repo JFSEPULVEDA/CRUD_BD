9d25610e2be1d344ef2ef19b73584e7755161ebc
JFSEPULVEDA <127802988+JFSEPULVEDA@users.noreply.github.com>
2024-05-15 19:37:37 -0500

    cambiode conexion database

--- src/database.js
+++ src/database.js
@@ -1,3 +1,8 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/stech', {
+/*mongoose.connect('mongodb://localhost:27017/stech', {
+});*/
+
+
+mongoose.connect('mongodb://mongo:bYLBDKiOzJKMfcBJfneqGddQgLJcrtFh@roundhouse.proxy.rlwy.net:29120', {
 });
+
